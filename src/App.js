1c353b44c1d43caa42373bfd2ac509efe846d3b1
Ronald George <ronaldsamgeorge@gmail.com>
2022-11-11 23:51:29 +0530

    Fetch data and display list of Country names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
 const App = () => {
+
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState('');
+
+  const updateSearchCountry = (event) => {
+    console.log(event.target.value);
+    setSearchCountry(event.target.value);
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v2/all')
+      .then(response => {
+        setCountries(response.data);
+      })
+  },[]);
+
+
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Search Country</h1>
+      Search : <input value={searchCountry} onChange={updateSearchCountry}/>
+
+      <ul>
+        {countries.map(country => <li key={country.name}>{country.name}</li>)}
+      </ul>
     </div>
   )
 }
